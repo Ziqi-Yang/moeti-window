e3c531d9218a2720f06e26d12784131a25e9a025
Meow King <mr.meowking@anche.no>
2024-03-31 21:03:13 +0800

    init

---
+++ dual-window.el
@@ -0,0 +1,34 @@
+;;; dual-window.el --- Dual Window Editing -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Meow King <mr.meowking@anche.no>
+
+;; Version: 0.1.0
+;; Author: Meow King <mr.meowking@anche.no>
+;; Keywords: convenience
+;; URL: https://git.sr.ht/~meow_king/dual-window
+;; License: GNU General Public License >= 3
+;; Package-Requires: ()  ;FIXME: `package-lint-current-buffer'
+
+;; This file is NOT part of Emacs.
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Dual Window Editing TODO
+
+;;; Code:
+
+(provide 'dual-window)
+
+;;; dual-window.el ends here
---
+++ justfile
@@ -0,0 +1,5 @@
+lint:
+    ./makem.sh lint
+
+lint-29:
+    ./makem.sh lint -E ~/myBin/emacs29.2
